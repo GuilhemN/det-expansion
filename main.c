ed285d1921ea32492181499ec2dedc8ed37a768a
Guilhem Niot <git@gniot.fr>
2024-01-22 22:21:51 +0100

    update

--- main.c
+++ main.c
@@ -75,7 +75,7 @@ static void local_nmod_mat_permute_rows(nmod_mat_t mat, const slong *perm_act,
  */
 void compute_minors(fmpz *minors, slong nb_minors, const fmpz_mat_t B)
 {
-    slong i, j, l, n;
+    slong i, j, l, n, l0;
     slong *P;
     mp_limb_t p;
     mp_ptr uimod, vimod;
@@ -107,195 +107,173 @@ void compute_minors(fmpz *minors, slong nb_minors, const fmpz_mat_t B)
     /* solve B_{1,..,n}^Tx = B_{n+1}^T */
     fmpq_mat_solve_fmpz_mat(x, Bt, dt);
 
-    int can_use_clever = 1;
-    for (l = 1; l < nb_minors;
-         l++)
-    { // Check that the last coordinates of x are non-zero as we need to
-      // divide by them in the clever method
-        can_use_clever &= !fmpq_is_zero(fmpq_mat_entry(x, n - l, 0));
-    }
+    fmpz_init(bound);
+    fmpz_init(prod);
+    fmpz_init(t);
+    fmpz_init(s1);
+    fmpz_init(s2);
+    ui = _fmpz_vec_init(nb_minors); // Will contain dividers of each minor
+    vi = _fmpz_vec_init(nb_minors);
+    uimod = _nmod_vec_init(nb_minors);
+    vimod = _nmod_vec_init(nb_minors);
 
-    int aborted = 0;
-    if (can_use_clever)
+    /* compute lcm of denominators of vectors x and ys */
+    fmpq_init(tmpq);
+    for (l = 0; l < nb_minors; l++)
     {
-        fmpz_init(bound);
-        fmpz_init(prod);
-        fmpz_init(t);
-        fmpz_init(s1);
-        fmpz_init(s2);
-        ui = _fmpz_vec_init(nb_minors); // Will contain dividers of each minor
-        vi = _fmpz_vec_init(nb_minors);
-        uimod = _nmod_vec_init(nb_minors);
-        vimod = _nmod_vec_init(nb_minors);
-
-        /* compute lcm of denominators of vectors x and ys */
-        fmpq_init(tmpq);
-        for (l = 0; l < nb_minors; l++)
-        {
-            fmpz_one(&ui[l]);
-        }
-        for (i = 0; i < n; i++)
-        {
-            fmpz_lcm(&ui[0], &ui[0], fmpq_mat_entry_den(x, i, 0));
-            for (l = 1; l < nb_minors; l++)
-            {
-                fmpq_div(tmpq, fmpq_mat_entry(x, i, 0), fmpq_mat_entry(x, n - l, 0));
-                fmpz_lcm(&ui[l], &ui[l], fmpq_denref(tmpq));
-            }
-        }
+        fmpz_one(&ui[l]);
+    }
+    for (i = 0; i < n; i++)
+    {
+        fmpz_lcm(&ui[0], &ui[0], fmpq_mat_entry_den(x, i, 0));
         for (l = 1; l < nb_minors; l++)
         {
-            fmpq_inv(tmpq, fmpq_mat_entry(x, n - l, 0));
+            if (fmpq_is_zero(fmpq_mat_entry(x, n - l, 0)))
+                continue;
+
+            fmpq_div(tmpq, fmpq_mat_entry(x, i, 0), fmpq_mat_entry(x, n - l, 0));
             fmpz_lcm(&ui[l], &ui[l], fmpq_denref(tmpq));
         }
-        fmpq_clear(tmpq);
+    }
+    for (l = 1; l < nb_minors; l++)
+    {
+        if (fmpq_is_zero(fmpq_mat_entry(x, n - l, 0)))
+            continue;
 
-        /* compute Hadamard bounds */
-        /* Slight overestimate as we could remove one of the columns to bound each
-         * minor */
-        fmpz_one(s2);
-        for (j = 0; j < n + 1; j++)
+        fmpq_inv(tmpq, fmpq_mat_entry(x, n - l, 0));
+        fmpz_lcm(&ui[l], &ui[l], fmpq_denref(tmpq));
+    }
+    fmpq_clear(tmpq);
+
+    /* compute Hadamard bounds */
+    /* Slight overestimate as we could remove one of the columns to bound each
+        * minor */
+    fmpz_one(s2);
+    for (j = 0; j < n + 1; j++)
+    {
+        fmpz_zero(s1);
+        for (i = 0; i < n; i++)
+            fmpz_addmul(s1, fmpz_mat_entry(Bt, i, j), fmpz_mat_entry(Bt, i, j));
+        fmpz_sqrtrem(s1, t, s1);
+        if (!fmpz_is_zero(t))
+            fmpz_add_ui(s1, s1, UWORD(1));
+        fmpz_mul(s2, s2, s1);
+    }
+
+    for (l = 0; l < nb_minors; l++)
+    {
+        fmpz_cdiv_q(t, s2, &ui[l]);
+        if (fmpz_cmp(bound, t) < 0)
         {
-            fmpz_zero(s1);
-            for (i = 0; i < n; i++)
-                fmpz_addmul(s1, fmpz_mat_entry(Bt, i, j), fmpz_mat_entry(Bt, i, j));
-            fmpz_sqrtrem(s1, t, s1);
-            if (!fmpz_is_zero(t))
-                fmpz_add_ui(s1, s1, UWORD(1));
-            fmpz_mul(s2, s2, s1);
+            fmpz_set(bound, t);
         }
+    }
+    // We need a bigger bound to recover the sign
+    fmpz_mul_ui(bound, bound, UWORD(2));
 
+    fmpz_one(prod);
+    P = _perm_init(n);
+    nmod_mat_init(Btmod, n, n, 2);
+    nmod_mat_init(dtmod, n, 1, 2);
+    p = UWORD(1) << NMOD_MAT_OPTIMAL_MODULUS_BITS;
+    /* compute determinants divided by u1 and u2 */
+    while (fmpz_cmp(prod, bound) <= 0)
+    {
+        p = n_nextprime(p, 0);
+
+        // If p divides one the divisors, skip it
+        int cont = 1;
         for (l = 0; l < nb_minors; l++)
         {
-            fmpz_cdiv_q(t, s2, &ui[l]);
-            if (fmpz_cmp(bound, t) < 0)
+            uimod[l] = fmpz_fdiv_ui(&ui[l], p);
+            if (!uimod[l])
             {
-                fmpz_set(bound, t);
+                cont = 0;
+                break;
             }
         }
-        // We need a bigger bound to recover the sign
-        fmpz_mul_ui(bound, bound, UWORD(2));
+        if (!cont) continue;
 
-        fmpz_one(prod);
-        P = _perm_init(n);
-        nmod_mat_init(Btmod, n, n, 2);
-        nmod_mat_init(dtmod, n, 1, 2);
-        p = UWORD(1) << NMOD_MAT_OPTIMAL_MODULUS_BITS;
-        /* compute determinants divided by u1 and u2 */
-        while (fmpz_cmp(prod, bound) <= 0)
-        {
-            p = n_nextprime(p, 0);
-            int cont = 1;
-            for (l = 0; l < nb_minors; l++)
-            {
-                uimod[l] = fmpz_fdiv_ui(&ui[l], p);
-                if (!uimod[l])
-                {
-                    cont = 0;
-                    break;
-                }
-            }
+        _nmod_mat_set_mod(Btmod, p);
+        _nmod_mat_set_mod(dtmod, p);
 
-            if (!cont)
-            {
-                continue;
-            }
+        for (l0 = 0; l0 < nb_minors; l0++) {
 
-            _nmod_mat_set_mod(Btmod, p);
-            _nmod_mat_set_mod(dtmod, p);
-
-            // We start by computing the LU decomposition of the matrix without its last column
+            // We start by computing the LU decomposition of the matrix without its n-l0 column
             // Copy the entire matrix except last column
             for (i = 0; i < n; i++) {
-                for (j = 0; j < n; j++)
+                for (j = 0; j < n-l0; j++)
+                    nmod_mat_entry(Btmod, i, j) = fmpz_fdiv_ui(fmpz_mat_entry(B, i, j), p);
+                for (j = n-l0+1; j <= n; j++)
                     nmod_mat_entry(Btmod, i, j) = fmpz_fdiv_ui(fmpz_mat_entry(B, i, j), p);
             }
 
             nmod_mat_lu(P, Btmod, 0);
 
-            // We can already compute minors[0]
-            vimod[0] = UWORD(1);
+            // We can already compute minors[l0]
+            vimod[l0] = UWORD(1);
             for (i = 0; i < n; i++)
-                vimod[0] = n_mulmod2_preinv(vimod[0], nmod_mat_entry(Btmod, i, i), p,
-                                            Btmod->mod.ninv);
+                vimod[l0] = n_mulmod2_preinv(vimod[l0], nmod_mat_entry(Btmod, i, i), p,
+                                                Btmod->mod.ninv);
             if (_perm_parity(P, n) == 1)
-                vimod[0] = nmod_neg(vimod[0], Btmod->mod);
-            
-            // In case A is rank deficient mod p, we cannot apply our algorithm and need to abort
-            if (vimod[0] == 0) {
-                // In case this is the first prime, it's possible m_0 is zero, and we just abort
-                if (fmpz_is_zero(prod)) {
-                    aborted = 1;
-                    break;
-                }
+                vimod[l0] = nmod_neg(vimod[l0], Btmod->mod);
 
-                // else, continue with other primes
+            // In case A is rank deficient mod p, we cannot directly compute all the minors
+            if (vimod[l0] == 0) {
                 continue;
             }
 
-            // Then, compute the other minors
-            // Compute the coordinates of the last vector in the basis
-            for (i = 0; i < n; i++)
-            {
-                nmod_mat_entry(dtmod, i, 0) =
-                    fmpz_fdiv_ui(fmpz_mat_entry(B, i, n), p); // Column n-l
-            }
-            local_nmod_mat_permute_rows(dtmod, P, NULL);
-            nmod_mat_solve_tril(dtmod, Btmod, dtmod, 1);
-            nmod_mat_solve_triu(dtmod, Btmod, dtmod, 0);
-
-            for (int l = 1; l < nb_minors; l++)
-            {
-                vimod[l] = n_mulmod2_preinv(vimod[0], nmod_mat_entry(dtmod, n - l, 0), p,
-                                            Btmod->mod.ninv);
-                if (l % 2 == 0) {
-                    vimod[l] = nmod_neg(vimod[l], Btmod->mod);
-                }
-            }
-
-            // Update CRT
-            for (int l = 0; l < nb_minors; l++) {
-                vimod[l] = n_mulmod2_preinv(vimod[l], n_invmod(uimod[l], p),
-                                                p, Btmod->mod.ninv);
-                fmpz_CRT_ui(&vi[l], &vi[l], prod, vimod[l], p, 1);
-            }
-
-            fmpz_mul_ui(prod, prod, p);
+            break;
         }
 
-        for (l = 0; l < nb_minors; l++)
+        // Then, compute the other minors
+        // Compute the coordinates of the last vector in the basis
+        for (i = 0; i < n; i++)
         {
-            fmpz_mul(&minors[l], &ui[l], &vi[l]);
+            nmod_mat_entry(dtmod, i, 0) =
+                fmpz_fdiv_ui(fmpz_mat_entry(B, i, n - l0), p); // Column n-l
+        }
+        local_nmod_mat_permute_rows(dtmod, P, NULL);
+        nmod_mat_solve_tril(dtmod, Btmod, dtmod, 1);
+        nmod_mat_solve_triu(dtmod, Btmod, dtmod, 0);
+
+        for (int l = l0 + 1; l < nb_minors; l++)
+        {
+            vimod[l] = n_mulmod2_preinv(vimod[l0], nmod_mat_entry(dtmod, n - l, 0), p,
+                                        Btmod->mod.ninv);
+            if ((l - l0) % 2 == 0)
+            {
+                vimod[l] = nmod_neg(vimod[l], Btmod->mod);
+            }
         }
 
-        fmpz_clear(bound);
-        fmpz_clear(prod);
-        fmpz_clear(s1);
-        fmpz_clear(s2);
-        fmpz_clear(t);
-        _fmpz_vec_clear(ui, nb_minors);
-        _fmpz_vec_clear(vi, nb_minors);
-        _nmod_vec_clear(uimod);
-        _nmod_vec_clear(vimod);
-        _perm_clear(P);
-        nmod_mat_clear(Btmod);
-        nmod_mat_clear(dtmod);
+        // Update CRT
+        for (int l = 0; l < nb_minors; l++) {
+            vimod[l] = n_mulmod2_preinv(vimod[l], n_invmod(uimod[l], p),
+                                            p, Btmod->mod.ninv);
+            fmpz_CRT_ui(&vi[l], &vi[l], prod, vimod[l], p, 1);
+        }
+
+        fmpz_mul_ui(prod, prod, p);
     }
 
-    if (!can_use_clever || aborted) /* can't use the clever method above so naively compute the dets */
+    for (l = 0; l < nb_minors; l++)
     {
-        for (l = 0; l < nb_minors; l++)
-        {
-            if (l != 0)
-            {
-                // Replace column n-l by column n-l+1
-                for (j = 0; j < n; j++)
-                    fmpz_set(fmpz_mat_entry(Bt, j, n - l),
-                             fmpz_mat_entry(B, j, n - l + 1));
-            }
-            fmpz_mat_det(&minors[l], Bt);
-        }
+        fmpz_mul(&minors[l], &ui[l], &vi[l]);
     }
+
+    fmpz_clear(bound);
+    fmpz_clear(prod);
+    fmpz_clear(s1);
+    fmpz_clear(s2);
+    fmpz_clear(t);
+    _fmpz_vec_clear(ui, nb_minors);
+    _fmpz_vec_clear(vi, nb_minors);
+    _nmod_vec_clear(uimod);
+    _nmod_vec_clear(vimod);
+    _perm_clear(P);
+    nmod_mat_clear(Btmod);
+    nmod_mat_clear(dtmod);
 
     fmpz_mat_clear(dt);
     fmpz_mat_clear(Bt);
@@ -322,9 +300,6 @@ int main()
     compute_minors(minors, nbminors, B);
 
     // for (int i = 0; i < nbminors; i++) {
-    //     fmpz_t det;
-    //     compute_minor(B, n-i, det);
-    //     printf("\n det %d ", i);
-    //     fmpz_print(det);
+    //     fmpz_print(&minors[i]);
     // }
 }
